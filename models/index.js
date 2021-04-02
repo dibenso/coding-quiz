70292570295c410403f39070206113a35d0095e7
Dillon Benson <dibenso93@gmail.com>
2021-04-01 22:50:12 -0400

    fix typo in model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
 const ScoreSchema = new mongoose.Schema({ score: "Number", initials: "String" });
-const Score = new mongoose.model("Workout", ScoreSchema);
+const Score = new mongoose.model("Score", ScoreSchema);
 
 module.exports = { Score };
